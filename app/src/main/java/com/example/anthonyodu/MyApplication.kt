c8e00c9f8c06da1f367e80805fb2a88d08c415dd
Odu Anthony <teerox44@gmail.com>
2020-06-06 22:33:29 +0100

    Implement SSlContext in the Application class

--- app/src/main/java/com/example/anthonyodu/MyApplication.kt
+++ app/src/main/java/com/example/anthonyodu/MyApplication.kt
@@ -4,6 +4,11 @@ import android.app.Application
 import com.example.anthonyodu.di.ApplicationComponent
 import com.example.anthonyodu.di.DaggerApplicationComponent
 import com.example.anthonyodu.di.module.NetworkModule
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException
+import com.google.android.gms.common.GooglePlayServicesRepairableException
+import com.google.android.gms.security.ProviderInstaller
+import java.security.NoSuchAlgorithmException
+import javax.net.ssl.SSLContext
 
 class MyApplication: Application() {
     // Reference to the application graph that is used across the whole app
@@ -11,6 +16,7 @@ class MyApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        initializeSSLContext()
         component = DaggerApplicationComponent.builder()
             .networkModule(NetworkModule(this))
             .build()
@@ -18,5 +24,22 @@ class MyApplication: Application() {
 
     fun getSharedComponent(): ApplicationComponent {
         return component
+    }
+
+
+    private fun initializeSSLContext() {
+        try {
+            SSLContext.getInstance("TLSv1.2");
+        } catch (e: NoSuchAlgorithmException) {
+            e.printStackTrace()
+        }
+
+        try {
+            ProviderInstaller.installIfNeeded(this)
+        } catch (e: GooglePlayServicesRepairableException) {
+            e.printStackTrace()
+        } catch (e: GooglePlayServicesNotAvailableException) {
+            e.printStackTrace()
+        }
     }
 }
